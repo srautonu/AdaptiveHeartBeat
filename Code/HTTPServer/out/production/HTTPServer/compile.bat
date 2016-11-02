229a2d127da074c76852f997300f9cf05071b881
Saifur Rahman <saifur80@gmail.com>
2016-11-02 11:09:46 +0600

    Removing files from /out folder. They got synced by mistake.

--- Code/HTTPServer/out/production/HTTPServer/compile.bat
+++
@@ -1,3 +0,0 @@
-set classpath=C:\Users\mrahman\Desktop\HTTPServer\appengine-api-1.2.0.jar;C:\Users\mrahman\Desktop\HTTPServer\httpclient-4.5.jar;C:\Users\mrahman\Desktop\HTTPServer\httpcore-4.4.1.jar;C:\Users\mrahman\Desktop\HTTPServer\org-apache-commons-logging.jar;C:\Users\mrahman\Desktop\HTTPServer\org.json.jar;
-rem javac C:\Users\mrahman\Desktop\HTTPServer\src\NotificationGenerator.java
-javac C:\Users\mrahman\Desktop\HTTPServer\src\JavaApplication1.java
